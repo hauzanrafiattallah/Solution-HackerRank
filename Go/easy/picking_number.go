4a26fce43548f074f1152415d0524d042697406f
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-08-12 21:53:35 +0700

    chore: Refactor pickingNumbers function for improved performance and readability

--- Go/easy/picking_number.go
+++ Go/easy/picking_number.go
@@ -1,25 +1,35 @@
+package main
+
+import "fmt"
+
 func pickingNumbers(a []int32) int32 {
 	var maxLength int32
 	n := len(a)
-	
+
 	for i := 0; i < n; i++ {
-			var count1, count2 int32
-			for j := 0; j < n; j++ {
-					if a[j] == a[i] || a[j] == a[i]+1 {
-							count1++
-					}
-					if a[j] == a[i] || a[j] == a[i]-1 {
-							count2++
-					}
+		var count1, count2 int32
+		for j := 0; j < n; j++ {
+			if a[j] == a[i] || a[j] == a[i]+1 {
+				count1++
 			}
-			if count1 > maxLength {
-					maxLength = count1
+			if a[j] == a[i] || a[j] == a[i]-1 {
+				count2++
 			}
-			if count2 > maxLength {
-					maxLength = count2
-			}
+		}
+		if count1 > maxLength {
+			maxLength = count1
+		}
+		if count2 > maxLength {
+			maxLength = count2
+		}
 	}
-	
+
 	return maxLength
 
+}
+
+func main() {
+	// Test case 1
+	a := []int32{4, 6, 5, 3, 3, 1}
+	fmt.Println(pickingNumbers(a)) // 3
 }
